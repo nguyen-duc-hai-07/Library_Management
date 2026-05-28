e2ce3743cd4ad1014d293862c10ee20289d3581d
Haziii1 <nguyenduchaikm1@gmail.com>
2026-05-28 14:00:12 +0700

    refactor: remove hard deleted

--- src/main/java/com/library/service/BorrowService.java
+++ src/main/java/com/library/service/BorrowService.java
@@ -1,6 +1,5 @@
 package com.library.service;
 
-import com.library.dto.request.BookFilterRequest;
 import com.library.dto.request.BorrowFilterRequest;
 import com.library.dto.request.BorrowRequest;
 import com.library.dto.response.BorrowResponse;
@@ -8,15 +7,13 @@ import com.library.dto.response.BorrowResponse;
 import java.util.List;
 
 public interface BorrowService {
-    BorrowResponse borrowBook(BorrowRequest borrowRequest) throws Exception;
+    BorrowResponse borrowBook(BorrowRequest borrowRequest);
 
-    List<BorrowResponse> filter(BorrowFilterRequest filter) throws Exception;
+    List<BorrowResponse> filter(BorrowFilterRequest filter);
 
-    BorrowResponse viewBorrowById(int id) throws Exception;
+    BorrowResponse viewBorrowById(int id);
 
-    void deleteBorrow(int id) throws Exception;
+    void softDeleteBorrow(int id);
 
-    void softDeleteBorrow(int id) throws Exception;
-
-    BorrowResponse returnBook(int id) throws Exception;
+    BorrowResponse returnBook(int id);
 }
