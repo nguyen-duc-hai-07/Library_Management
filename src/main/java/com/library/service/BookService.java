2e304ea865db938632e3a4ebcd933e7bf05672ec
Haziii1 <nguyenduchaikm1@gmail.com>
2026-05-30 21:40:40 +0700

    feat: add getAvailabeBookOrThrow and updateStock

--- src/main/java/com/library/service/BookService.java
+++ src/main/java/com/library/service/BookService.java
@@ -4,21 +4,26 @@ import com.library.dto.request.BookFilterRequest;
 import com.library.dto.request.BookRequest;
 import com.library.dto.response.BookResponse;
 import com.library.dto.response.UserResponse;
+import com.library.model.Book;
 
 import java.util.List;
 
 public interface BookService {
-    BookResponse createBook(BookRequest bookRequest) throws Exception;
+    BookResponse createBook(BookRequest bookRequest) ;
 
-    List<BookResponse> filter(BookFilterRequest filter) throws Exception;
+    List<BookResponse> filter(BookFilterRequest filter) ;
 
-    BookResponse viewBookById(int id) throws Exception;
+    BookResponse viewBookById(int id) ;
 
-    BookResponse updateBook(int id, BookRequest bookRequest) throws Exception;
+    BookResponse updateBook(int id, BookRequest bookRequest) ;
 
-    void softDeleteBook(int id) throws Exception;
+    void softDeleteBook(int id) ;
 
-    List<UserResponse> viewAllUsersByBook(int bookId) throws Exception;
+    List<UserResponse> viewAllUsersByBook(int bookId) ;
 
-    BookResponse updateQuantity(int id, int quantity) throws Exception;
+    BookResponse updateQuantity(int id, int quantity) ;
+
+    Book getAvailableBookOrThrow(int bookId);
+
+    void updateStock(int bookId, int quantity);
 }
